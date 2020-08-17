6e3adba871d947e81cd488fa975426a64ec08286
Vinithius <mfilho@morphus.com.br>
2020-08-17 10:42:47 -0300

    Rename

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oowlish.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customer-project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
